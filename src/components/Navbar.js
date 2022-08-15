8bfbb7fd40299f80a85f72b5f5ceb2c3f7b4f78a
dhaferslimi23 <47641213+dhaferslimi23@users.noreply.github.com>
2022-08-15 07:56:02 +0100

    Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -81,7 +81,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/dhaferslimi23/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
