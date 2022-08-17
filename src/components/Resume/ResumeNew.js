2e046119e267ee4f502be0f0b0ae02f7bf3c6891
Dhafer Slimi <dhafer.slimi@esprit.tn>
2022-08-17 08:34:02 +0100

    Commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/CV_Dhafer_Slimi.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
@@ -19,20 +18,8 @@ function ResumeNew() {
 
   return (
     <div>
-      <Container fluid className="resume-section">
-        <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
+      <Container fluid className="home-section">
+        
         <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
@@ -43,8 +30,9 @@ function ResumeNew() {
           <Button
             variant="primary"
             href={pdf}
+            className="purple"
             target="_blank"
-            style={{ maxWidth: "250px" }}
+            style={{ maxWidth: "250px"} }
           >
             <AiOutlineDownload />
             &nbsp;Download CV
